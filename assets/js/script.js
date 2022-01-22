baed60b4c1c5250236dea9eda9d98cfa140d884c
bgould86 <bgould86@gmail.com>
2022-01-22 10:11:18 -0600

    fetch works

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,16 @@
 
 // functions
 //call Open Library with the genre and get that cream filling
+
+function getStuff() {
+  fetch("http://openlibrary.org/search.json?subject=fantasy&limit=10")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
 //display results
 
 //filter results based on page number (lodash)
@@ -10,3 +20,4 @@
 // event listeners
 //submit for genre
 //filter(s)
+getStuff();
